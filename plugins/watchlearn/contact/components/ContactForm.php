a6f705d2383b8442dfa14e7890380122878c013f
gyanyim <gyanyi.miklos@gmail.com>
2021-03-19 15:57:47 +0100

    My first OctoberCMS project.

---
+++ plugins/watchlearn/contact/components/ContactForm.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Watchlearn\Contact\Components;
+
+use Cms\Classes\ComponentBase;
+use Input;
+use Mail;
+use Validator;
+use Redirect;
+
+class ContactForm extends ComponentBase
+{
+
+    public function componentDetails()
+    {
+        return [
+            'name' => 'Contact Form',
+            'description' => 'Simple contact form'
+        ];
+    }
+
+    public function onSend()
+    {
+
+        $validator = Validator::make(
+            [
+                'name' => Input::get('name'),
+                'email' => Input::get('email')
+            ],
+            [
+                'name' => 'required|min:5',
+                'email' => 'required|email'
+            ]
+        );
+
+        if ($validator->fails()) {
+
+            return ['#result' => $this->renderPartial('contactform::messages', [
+                'errorMsgs' => $validator->messages()->all(),
+                'fieldMsgs' => $validator->messages()
+            ])];
+
+        } else {
+
+
+            $vars = [
+                'name' => Input::get('name'), 'email' => Input::get('email'),
+                'content' => Input::get('content')
+            ];
+
+            Mail::send('watchlearn.contact::mail.message', $vars, function ($message) {
+
+                $message->to('admin@domain.tld', 'Admin Person');
+                $message->subject('New message from contact form');
+            });
+        }
+    }
+}
---
+++ plugins/watchlearn/movies/updates/builder_table_update_watchlearn_movies_actors_2.php
@@ -0,0 +1,23 @@
+<?php namespace Watchlearn\Movies\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdateWatchlearnMoviesActors2 extends Migration
+{
+    public function up()
+    {
+        Schema::table('watchlearn_movies_actors', function($table)
+        {
+            $table->renameColumn('last_name', 'lastname');
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('watchlearn_movies_actors', function($table)
+        {
+            $table->renameColumn('lastname', 'last_name');
+        });
+    }
+}
